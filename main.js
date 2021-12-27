bd8ca2071b7b14cb09c3457d186a0455fbc8b3ea
rahulnishad96 <rahulnishadhandia@gmail.com>
2021-12-27 11:56:29 +0530

    final commit

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const colordiv = document.getElementById("colordiv");
-const textinput =  document.getElementById("start-input");
-console.log(colordiv + textinput);
-console.log(textinput);
+const myBtn =  document.getElementById("my-btn");
+//console.log(colordiv + textinput);
+console.log(myBtn);
 
-
+let myArr=[];
 colordiv.style.backgroundColor="blue";
 let x=0;
 let previndex=5;
@@ -17,13 +17,28 @@ function everyTime() {
     }
     previndex=index;
     console.log(colorArray[previndex]);
+    myArr.push(colorArray[previndex]);
     colordiv.style.backgroundColor=colorArray[index];
     x++;
-    if(x==16)
+    if(x==16){
         clearInterval(myInterval);
+        console.log(myArr);
+         myArr.map((item,index) => {
+            var name = document.createElement('div');
+            name.innerHTML=index + " : "+ item ;
+            name.style.color=item;
+           
+            
+            document.getElementById("colDiv").appendChild(name);
+        });
+        console.log(document.getElementById("colDiv"));
+    }
 
 }
-function myKeyUpFunction(){
+function clickHandlar(){
     console.log("function called");
-    myInterval = setInterval(everyTime, 1000);
+    myBtn.style.display="none";
+    const time =  document.getElementById("time").value;
+    console.log(time);
+    myInterval = setInterval(everyTime,time || 1000);
 }
